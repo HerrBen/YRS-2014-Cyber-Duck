95157b1e342cbb6da3bd9b76f0ad3ab9a2ce9d02
Harjot Singh <harjot@harkul.com>
2014-07-29 23:13:23 +0100

    Geolocation sorted - HTML document body now contained in hidden container UNLESS location is available. Flipclock is working (to some extent, a trial run) Main.js now grabs geolocation and can calculate correct sunset. More refractoring and modularization of code to be done (grab town and city via GMaps API)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
+//Main javascript with all the fancy stuffs
+
+var times;
+var latitude;
+var longitude;
+
+//Request location before doing anything
+window.onload = function(){
+	get_location();
+};
+
+function processNumber(){
+
+};
+
+function get_location() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+	} 
+};
+
+function successFunction(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+ //   codeLatLng(lat, lng);
+    // Get sunset time
+    times = SunCalc.getTimes(new Date(), latitude, longitude);
+    // Log in the console
+    console.log("Sunset: "+times.sunset);
+	document.getElementById("container").style.display = "block"; //Display HTML content
+}
+
+function errorFunction(){
+    alert("Sorry, you can't use this website, please enable location services");
+}
 
