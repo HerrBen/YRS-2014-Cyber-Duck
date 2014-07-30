09efb63d92755cba93de4b88bd9edf338bacebe6
Harjot Singh <harjot@harkul.com>
2014-07-30 11:56:16 +0100

    stuff

--- js/main.js
+++ js/main.js
@@ -9,10 +9,6 @@ window.onload = function(){
 	get_location();
 };
 
-function processNumber(){
-
-};
-
 function get_location() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
@@ -22,15 +18,19 @@ function get_location() {
 function successFunction(position) {
     latitude = position.coords.latitude;
     longitude = position.coords.longitude;
- //   codeLatLng(lat, lng);
+	//codeLatLng(lat, lng);
     // Get sunset time
     times = SunCalc.getTimes(new Date(), latitude, longitude);
-    // Log in the console
+    //Log in the console
     console.log("Sunset: "+times.sunset);
 	document.getElementById("container").style.display = "block"; //Display HTML content
 }
+
 
 function errorFunction(){
     alert("Sorry, you can't use this website, please enable location services");
 }
 
+function processNumber(){
+	console.log("Hello :" + times.sunset);
+};
