c4058852f3a2d9955e3bd0ccaa083c6042df9454
Jake Sieradzki <jacob.sieradzki@gmail.com>
2014-08-01 11:43:44 +0100

    Stuff

--- js/main.js
+++ js/main.js
@@ -74,6 +74,7 @@ function successFunction(position) {
 	getLocation(latitude, longitude);	//Grab location and set it
 	getSetTimes(); //Get and set clocks, sunset and sunrise times
 	$("#container").show();
+	$(".overlay").fadeOut('fast');
 }
 
 function getLocation(lat, lng) {
